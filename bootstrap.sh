566928c3b206260ae360987b018bb1bab52d934c
Simon Gene Gottlieb <gene@staubsaal.de>
2015-08-04 18:21:31 +0200

    adding bootstrap.sh script

---
+++ bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+mkdir packages
+git clone ../JsonSerializer.git -b aBuild packages/JsonSerializer
+git clone ../jsoncpp.git        -b aBuild packages/jsoncpp
+clang++ -ggdb -O0 --std=c++11 \
+	-isystem packages/JsonSerializer/src/ \
+	-isystem packages/jsoncpp/include \
+	src/aBuild/*.cpp \
+	packages/jsoncpp/src/lib_json/json_reader.cpp \
+	packages/jsoncpp/src/lib_json/json_value.cpp \
+	packages/jsoncpp/src/lib_json/json_writer.cpp \
+	-o aBuild
