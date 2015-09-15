24a7a00723e2ba88ea697cc9d17f57a5e7724f61
Simon Gene Gottlieb <gene@staubsaal.de>
2015-09-16 00:49:54 +0200

    fixing bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,13 +2,15 @@
 mkdir packages
 git clone git@78.47.95.250:aBuild/JsonSerializer  packages/JsonSerializer
 git clone git@78.47.95.250:aBuild/jsoncpp         packages/jsoncpp
-clang++ -ggdb -O0 --std=c++11 \
+g++ -ggdb -O0 --std=c++11 \
 	-isystem packages/JsonSerializer/src/ \
 	-isystem packages/jsoncpp/include \
 	src/aBuild/*.cpp \
+	packages/JsonSerializer/src/jsonSerializer/jsonSerializer.cpp \
 	packages/jsoncpp/src/lib_json/json_reader.cpp \
 	packages/jsoncpp/src/lib_json/json_value.cpp \
 	packages/jsoncpp/src/lib_json/json_writer.cpp \
+	-lpthread \
 	-o aBuild
 ./aBuild
 ./aBuild test
