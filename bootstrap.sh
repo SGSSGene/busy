b7c997e34da1cd9b1c34b6985dec16477fd98108
Simon Gene Gottlieb <gene@staubsaal.de>
2015-12-17 02:51:48 +0100

    upating bootstrap process

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,7 @@ git clone git@cakeface.de:aBuild/Serializer      packages/Serializer
 git clone git@cakeface.de:aBuild/jsoncpp         packages/jsoncpp
 git clone git@cakeface.de:aBuild/CommonOptions   packages/CommonOptions
 git clone git@cakeface.de:aBuild/ThreadPool      packages/ThreadPool
+git clone git@cakeface.de:aBuild/yaml-cpp        packages/yaml-cpp
 
 set -e
 
@@ -13,13 +14,17 @@ g++ -ggdb -O0 --std=c++11 \
 	-isystem packages/jsoncpp/include \
 	-isystem packages/CommonOptions/src \
 	-isystem packages/ThreadPool/src \
+	-isystem packages/yaml-cpp/include \
 	-I src/aBuild/ \
 	src/aBuild/*.cpp \
 	src/aBuild/commands/*.cpp \
-	packages/Serializer/src/serializer/*.cpp \
+	packages/Serializer/src/serializer/binary/*.cpp \
+	packages/Serializer/src/serializer/json/*.cpp \
+	packages/Serializer/src/serializer/yaml/*.cpp \
 	packages/jsoncpp/src/lib_json/json_reader.cpp \
 	packages/jsoncpp/src/lib_json/json_value.cpp \
 	packages/jsoncpp/src/lib_json/json_writer.cpp \
+	packages/yaml-cpp/src/yaml-cpp/*.cpp \
 	-lpthread \
 	-o aBuild
 ./aBuild --build aBuild
