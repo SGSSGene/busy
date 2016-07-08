c1cfce89c430b6de99764d7f13ddd9280eb236eb
Simon Gene Gottlieb <gene@staubsaal.de>
2016-07-08 11:59:16 +0200

    adjusting bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,36 +1,39 @@
 #!/bin/bash
 
 mkdir -p packages
-git clone git@cakeface.de:aBuild/Serializer      packages/Serializer
-git clone git@cakeface.de:aBuild/Process         packages/Process
-git clone git@cakeface.de:aBuild/jsoncpp         packages/jsoncpp
-git clone git@cakeface.de:aBuild/CommonOptions   packages/CommonOptions
-git clone git@cakeface.de:aBuild/ThreadPool      packages/ThreadPool
-git clone git@cakeface.de:aBuild/yaml-cpp        packages/yaml-cpp
+git clone git@cakeface.de:aBuild/Serializer      extRepositories/Serializer
+git clone git@cakeface.de:aBuild/Process         extRepositories/Process
+git clone git@cakeface.de:aBuild/jsoncpp         extRepositories/jsoncpp
+git clone git@cakeface.de:aBuild/CommonOptions   extRepositories/CommonOptions
+git clone git@cakeface.de:aBuild/ThreadPool      extRepositories/ThreadPool
+git clone git@cakeface.de:aBuild/yaml-cpp        extRepositories/yaml-cpp
 
 set -e
 
 g++ -ggdb -O0 --std=c++11 \
-	-isystem packages/Serializer/src/ \
-	-isystem packages/jsoncpp/include \
-	-isystem packages/CommonOptions/src \
-	-isystem packages/ThreadPool/src \
-	-isystem packages/yaml-cpp/include \
-	-isystem packages/Process/src \
+	-isystem extRepositories/Serializer/src/ \
+	-isystem extRepositories/jsoncpp/include \
+	-isystem extRepositories/CommonOptions/src \
+	-isystem extRepositories/ThreadPool/src \
+	-isystem extRepositories/yaml-cpp/include \
+	-isystem extRepositories/Process/src \
 	-I src/aBuild/ \
 	src/aBuild/*.cpp \
 	src/aBuild/commands/*.cpp \
-	packages/Serializer/src/serializer/binary/*.cpp \
-	packages/Serializer/src/serializer/json/*.cpp \
-	packages/Serializer/src/serializer/yaml/*.cpp \
-	packages/jsoncpp/src/lib_json/json_reader.cpp \
-	packages/jsoncpp/src/lib_json/json_value.cpp \
-	packages/jsoncpp/src/lib_json/json_writer.cpp \
-	packages/yaml-cpp/src/yaml-cpp/*.cpp \
-	packages/CommonOptions/src/commonOptions/*.cpp \
-	packages/Process/src/process/*.cpp \
+	extRepositories/Serializer/src/serializer/binary/*.cpp \
+	extRepositories/Serializer/src/serializer/json/*.cpp \
+	extRepositories/Serializer/src/serializer/yaml/*.cpp \
+	extRepositories/jsoncpp/src/lib_json/json_reader.cpp \
+	extRepositories/jsoncpp/src/lib_json/json_value.cpp \
+	extRepositories/jsoncpp/src/lib_json/json_writer.cpp \
+	extRepositories/yaml-cpp/src/yaml-cpp/*.cpp \
+	extRepositories/CommonOptions/src/commonOptions/*.cpp \
+	extRepositories/Process/src/process/*.cpp \
 	-lpthread \
 	-o aBuild
 ./aBuild build aBuild
-sudo ./aBuild install
-rm ./aBuild
+
+echo "run:"
+echo "$ sudo ./aBuild install"
+echo "or copy build/system-gcc-4.9/aBuild to /usr/bin"
+
