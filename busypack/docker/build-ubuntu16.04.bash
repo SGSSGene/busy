40eb452a6008f174a4a0c3d4c163dd57723dc469
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2018-06-12 17:39:20 +0200

    fixing bash completion

--- busypack/docker/build-ubuntu16.04.bash
+++ busypack/docker/build-ubuntu16.04.bash
@@ -8,5 +8,5 @@ rm -rf root
 mkdir -p root/usr/bin
 mkdir -p root/usr/share/busy
 
-cp share/busy/completions/bash_completion root/usr/share/busy/busy
+cp share/busy/completions/bash_completion root/etc/bash_completion.d/busy
 cp build/fallback-gcc/debug/busy root/usr/bin/busy
