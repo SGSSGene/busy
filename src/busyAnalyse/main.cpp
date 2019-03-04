dff824570b19bc61bd80c6fd1f3b8c9e144bed89
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2019-03-04 14:45:50 +0100

    patch: move 'class Queue' into own file

--- src/busyAnalyse/main.cpp
+++ src/busyAnalyse/main.cpp
@@ -1,5 +1,6 @@
 #include "FileCache.h"
 #include "Package.h"
+#include "Queue.h"
 
 #include <process/Process.h>
 #include <algorithm>
@@ -7,9 +8,6 @@
 #include <filesystem>
 #include <fstream>
 #include <iostream>
-#include <list>
-#include <mutex>
-#include <queue>
 
 namespace {
 	auto isPrefix(std::filesystem::path const& _prefix, std::filesystem::path const& _path) -> bool {
@@ -332,128 +330,6 @@ auto readFullFile(std::filesystem::path const& file) {
 	ifs.read(reinterpret_cast<char*>(buffer.data()), buffer.size());
 	return buffer;
 }
-
-template <typename ...Args>
-class Queue {
-public:
-	using Node = std::variant<Args*...>;
-	using Edge = std::tuple<Node, Node>;
-	using Nodes = std::vector<Node>;
-	using Edges = std::vector<Edge>;
-
-private:
-
-	struct IntNode {
-		Node value;
-		int inNodeCt{0};
-		std::vector<IntNode*> inNode;
-		std::vector<IntNode*> outNode;
-	};
-
-	std::list<IntNode> nodes;
-	std::queue<IntNode*> work;
-	std::mutex mutex;
-
-	auto& findNode(Node v) {
-		for (auto& n : nodes) {
-			if (n.value == v) {
-				return n;
-			}
-		}
-		assert(false);
-	}
-public:
-	Queue(Nodes const& _nodes, Edges const& _edges) {
-		for (auto n : _nodes) {
-			nodes.emplace_back(IntNode{n, {}, {}});
-		}
-		for (auto [src, dst] : _edges) {
-			auto& srcNode = findNode(src);
-			auto& dstNode = findNode(dst);
-			srcNode.outNode.push_back(&dstNode);
-			dstNode.inNode.push_back(&srcNode);
-		}
-
-		for (auto& n : nodes) {
-			if (n.inNode.size() == n.inNodeCt) {
-				work.push(&n);
-			}
-		}
-	}
-
-	bool empty() const {
-		return work.empty();
-	}
-	template <typename T>
-	auto find_outgoing(Node n) -> T& {
-		auto& node = findNode(n);
-		for (auto const& n : node.outNode) {
-			if (std::holds_alternative<T*>(n->value)) {
-				return *std::get<T*>(n->value);
-			}
-		}
-		assert(false);
-	}
-	template <typename L>
-	void visit_incoming(Node n, L const& l) {
-		auto& node = findNode(n);
-
-		std::queue<IntNode*> work;
-
-		for (auto n : node.inNode) {
-			work.push(n);
-		}
-
-		while (not work.empty()) {
-			auto front = work.front();
-			work.pop();
-			std::visit([&](auto& ptr) {
-				l(*ptr);
-			}, front->value);
-			for (auto n : front->inNode) {
-				work.push(n);
-			}
-		}
-	}
-
-	template <typename T>
-	auto find_incoming(Node n) -> std::vector<T const*> {
-		std::vector<T const*> result;
-		auto& node = findNode(n);
-		for (auto const& n : node.inNode) {
-			if (std::holds_alternative<T const*>(n->value)) {
-				result.push_back(std::get<T const*>(n->value));
-			}
-		}
-		return result;
-	}
-
-
-
-	template <typename L>
-	void dispatch(L const& l) {
-		auto front = [&] {
-			auto g = std::lock_guard{mutex};
-			auto front = work.front();
-			work.pop();
-			return front;
-		}();
-
-		std::visit([&](auto ptr) {
-			l(*ptr);
-		}, front->value);
-
-		{
-			auto g = std::lock_guard{mutex};
-			for (auto n : front->outNode) {
-				n->inNodeCt += 1;
-				if (n->inNode.size() == n->inNodeCt) {
-					work.push(n);
-				}
-			}
-		}
-	}
-};
 
 int main(int argc, char const** argv) {
 	try {
