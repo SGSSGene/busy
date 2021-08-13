1170d330ee2bfe6410751b13b2c33149e275dacf
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2021-08-13 15:23:46 +0200

    [PATCH] remove all convert2/visit2 functions

--- src/flattensObjectsNeatly/utils.h
+++ src/flattensObjectsNeatly/utils.h
@@ -4,19 +4,12 @@
 #include "convert.h"
 #include "proxy.h"
 #include "Node.h"
-#include "Visitor.h"
 
 namespace fon {
 
 template <typename Cb, typename T>
 auto visit(Cb cb, T& obj) {
     auto node = Node{cb};
-    return node % obj;
-}
-
-template <typename Cb1, typename Cb2, typename T>
-auto visit2(Cb1 cb1, Cb2 cb2, T& obj) {
-    auto node = Visitor{cb1, cb2};
     return node % obj;
 }
 
@@ -99,88 +92,6 @@ void findPath(T const& input, BaseType* target, L l) {
         }
     }, input);
 }
-
-template <typename F, typename Cb1, typename Cb2, typename T>
-void filter2(Cb1 cb1, Cb2 cb2, T& obj) {
-    visit2([&]<typename Value>(auto& visitor, Value& obj) {
-        if constexpr(std::is_base_of_v<F, Value> or std::is_same_v<F, std::remove_const_t<Value>>) {
-            cb1(visitor, obj);
-        } else if constexpr(std::is_polymorphic_v<F> and std::is_polymorphic_v<Value>) {
-            auto* newObj = dynamic_cast<F*>(&obj);
-            if (newObj) {
-                cb1(visitor, newObj);
-            } else {
-                visitor.visit(obj);
-            }
-        } else {
-            visitor.visit(obj);
-        }
-    }, cb2, obj);
-}
-
-template <template <typename...> typename F, typename Cb, typename T>
-void filter2(Cb cb, T& obj) {
-    visit2([&]<typename Value>(auto& visitor, Value& obj) {
-        if constexpr (is_same_base_v<F, Value>) {
-            cb(visitor, obj);
-        }
-    }, [](auto key) {}, obj);
-}
-
-template <Type type, typename Cb, typename T>
-void filter2(Cb cb, T& obj) {
-    visit2([&]<typename Node>(Node& node, auto& obj) {
-        if constexpr (Node::type == type) {
-            cb(node, obj);
-        } else if constexpr (Node::is_owner) {
-            fon::convert(node, obj);
-        }
-    }, obj);
-}
-
-template <typename BaseType, typename T, typename L>
-void findPath2(T& input, BaseType* target, L l) {
-    bool found{false};
-
-    filter2<BaseType>([&]<typename Node>(Node& node, BaseType& obj) {
-        if (found) {
-            return;
-        }
-
-        if (&obj == target) {
-            l(node);
-            found = true;
-            return;
-        }
-
-        if constexpr (Node::is_owner) {
-            fon::convert(node, obj);
-        }
-    }, input);
-}
-
-template <typename BaseType, typename T, typename L>
-void findPath2(T const& input, BaseType* target, L l) {
-    bool found{false};
-
-    filter2<BaseType>([&]<typename Node>(Node& node, BaseType const& obj) {
-        if (found) {
-            return;
-        }
-
-        if (&obj == target) {
-            l(node);
-            found = true;
-            return;
-        }
-
-        if constexpr (Node::is_owner) {
-            fon::convert(node, obj);
-        }
-    }, input);
-}
-
-
 
 template <typename BaseType, typename ...Args, typename T, typename L>
 void findObj(T& input, std::string const& path, L l) {
