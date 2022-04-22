b2a56f91ea3689e89f21401c2b53432772d50e9e
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2022-04-22 18:14:17 +0200

    [fix] cli test

--- tests/all.sh
+++ tests/all.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-if [ ! -e bin/busy ]; then
-	echo "failed - bin/busy not found"
-	echo "please execute this file from any busy build directory"
-	exit 1
-fi
-BUSY="$(pwd)/bin/busy"
+#if [ ! -e bin/busy ]; then
+#	echo "failed - bin/busy not found"
+#	echo "please execute this file from any busy build directory"
+#	exit 1
+#fi
+#BUSY="$(pwd)/bin/busy"
+BUSY='busy'
 
 cd "${0%/*}"
 
@@ -14,7 +15,7 @@ cd "${0%/*}"
 	rm -rf testProject/build
 	mkdir -p testProject/build
 	cd testProject/build
-	$BUSY ../busy.yaml > /dev/null
+	$BUSY --toolchain "gcc 11.2" ../busy.yaml > /dev/null
 
 	if [ "$(bin/app)" != "Hello World" ]; then
 		echo "failed"
@@ -29,7 +30,7 @@ cd "${0%/*}"
 	rm -rf testProject/different-build
 	mkdir -p testProject/different-build
 	cd testProject/different-build
-	$BUSY ../busy.yaml > /dev/null
+	$BUSY --toolchain "gcc 11.2" ../busy.yaml > /dev/null
 
 	if [ "$(bin/app)" != "Hello World" ]; then
 		echo "failed"
@@ -43,7 +44,7 @@ cd "${0%/*}"
 	rm -rf external-build
 	mkdir -p external-build
 	cd external-build
-	$BUSY ../testProject/busy.yaml > /dev/null
+	$BUSY --toolchain "gcc 11.2" ../testProject/busy.yaml > /dev/null
 
 	if [ "$(bin/app)" != "Hello World" ]; then
 		echo "failed"
@@ -55,7 +56,7 @@ cd "${0%/*}"
 
 ( # check normal compilation of executable works  with build path given
 	rm -rf testProject/build
-	$BUSY --build testProject/build testProject/busy.yaml > /dev/null
+	$BUSY --toolchain "gcc 11.2" --build testProject/build testProject/busy.yaml > /dev/null
 
 	if [ "$(testProject/build/bin/app)" != "Hello World" ]; then
 		echo "failed"
@@ -67,7 +68,7 @@ cd "${0%/*}"
 
 ( # check compilation works under different build folder with build path given
 	rm -rf testProject/different-build
-	$BUSY --build testProject/different-build testProject/busy.yaml > /dev/null
+	$BUSY --toolchain "gcc 11.2" --build testProject/different-build testProject/busy.yaml > /dev/null
 
 	if [ "$(testProject/different-build/bin/app)" != "Hello World" ]; then
 		echo "failed"
@@ -79,7 +80,7 @@ cd "${0%/*}"
 
 ( # check compilation works outside of project path with build path given
 	rm -rf external-build
-	$BUSY --build external-build testProject/busy.yaml > /dev/null
+	$BUSY --toolchain "gcc 11.2" --build external-build testProject/busy.yaml > /dev/null
 
 	if [ "$(external-build/bin/app)" != "Hello World" ]; then
 		echo "failed"
