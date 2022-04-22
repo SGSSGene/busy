749b9b3e004e28e8266199e41074f4cc707ce684
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2022-04-22 17:32:21 +0200

    [fix] update bashScript

--- share/busy/toolchains/bashScript.sh
+++ share/busy/toolchains/bashScript.sh
@@ -36,21 +36,58 @@ fi
 
 outFile="bashScript.sh"
 
-
 if [ "$1" == "init" ]; then
     rootDir="$2"
     relPath="$(pwd)"
-    cat > ${outFile} <<-END
-        #!/bin/bash
-        cd ${relPath}
+cat > ${outFile} <<-END
+#!/bin/bash
 
-        if [ ! -e "external" ]; then
-            ln -s ${rootDir}/external external
-        fi
-        if [ ! -e "src" ]; then
-            ln -s ${rootDir}/src src
-        fi
-    END
+set -Eeuo pipefail
+
+cd ${relPath}
+rootDir="${rootDir}"
+
+if [ ! -e "external" ]; then
+    ln -s ${rootDir}/external external
+fi
+if [ ! -e "src" ]; then
+    ln -s ${rootDir}/src src
+fi
+function setupTranslationSet {
+    local tsName="${1}"
+    # setup translation set \${tsName}
+    rm -rf environments/\${tsName}/includes
+    mkdir -p environments/\${tsName}/includes/local
+    mkdir -p environments/\${tsName}/includes/system
+}
+function setupSystemIncludesPaths {
+    local tsName="\${1}"; shift
+    for f in \${@}; do
+        local i=0
+        target="environments/\${tsName}/includes/system/\$i/\$(basename "\${f}")"
+        while [ -e \${target} ]; do
+            i=\$(expr \$i + 1)
+            target="environments/\${tsName}/includes/system/\$i/\$(basename "\${f}")"
+        done
+
+        mkdir -p "environments/\${tsName}/includes/system/\$i"
+        ln -s "\$(realpath "\${rootDir}/\${f}" --relative-to "\$(dirname "\${target}")")" -T "\${target}"
+    done
+}
+function setupSystemIncludes {
+    local tsName="\${1}"; shift
+    local i=0
+    local target="environments/\${tsName}/includes/system/\$i"
+    local systemIncludes=""
+    while [ -d \${target} ]; do
+        systemIncludes="\${systemIncludes} -isystem \"\${target}\""
+        i=\$(expr \$i + 1)
+        target="environments/\${tsName}/includes/system/\$i"
+    done
+    systemIncludes=\$(echo \$systemIncludes | xargs -n 1 echo) # split arguments into singular arguments
+    echo \${systemIncludes}
+}
+END
     if [ ! -e "external" ]; then
         ln -s ${rootDir}/external external
     fi
@@ -62,13 +99,40 @@ if [ "$1" == "init" ]; then
     exit 0
 elif [ "$1" == "finalize" ]; then
     exit 0
+elif [ "$1" == "setup_translation_set" ] ; then
+    shift; rootDir="$1"
+    shift; tsName="$1"
+    shift;
+
+echo setupTranslationSet "${tsName}" >> ${outFile}
+
+parse "--ilocal  projectIncludes" \
+      "--isystem systemIncludes" \
+      "--verbose verbose" \
+      "--" "$@"
+
+
+for f in "${projectIncludes[@]}"; do
+cat >> ${outFile} <<-END
+    target="environments/${tsName}/includes/local/$(basename "${f}")"
+    ln -s "\$(realpath "${rootDir}/${f}" --relative-to "\$(dirname \${target}"))" -T "\${target}"
+END
+done
+
+if [ ${#systemIncludes} -gt 0 ]; then
+echo setupSystemIncludesPaths "${rootDir}" "${tsName}" "${systemIncludes[@]}" >> ${outFile}
+fi
+
+    exit 0
 elif [ "$1" == "compile" ]; then
+    shift; tsName="$1"
     shift; inputFile="$1"
     shift; outputFile="$1"
     shift
 
     parse "--ilocal  projectIncludes" \
           "--isystem systemIncludes" \
+          "--verbose verbose" \
           "--" "$@"
 
     projectIncludes+=($(dirname ${projectIncludes[-1]})) #!TODO this line should not be needed
@@ -77,12 +141,17 @@ elif [ "$1" == "compile" ]; then
 
     filetype="$(echo "${inputFile}" | rev | cut -d "." -f 1 | rev)";
     if [[ "${filetype}" =~ ^(cpp|cc)$ ]]; then
-        call="${CXX} -O0 -std=c++20 -fPIC -MD -g3 -ggdb -fdiagnostics-color=always -c $inputFile -o $outputFile $projectIncludes $systemIncludes"
+        call="${CXX} -O0 -std=c++20 -fPIC -MD -g3 -ggdb -fdiagnostics-color=always -c $inputFile -o $outputFile $projectIncludes $systemIncludes \$systemIncludes"
     elif [ "${filetype}" = "c" ]; then
-        call="${C} -O0 -std=c18 -fPIC -MD -g3 -ggdb -fdiagnostics-color=always -c $inputFile -o $outputFile $projectIncludes $systemIncludes"
+        call="${C} -O0 -std=c18 -fPIC -MD -g3 -ggdb -fdiagnostics-color=always -c $inputFile -o $outputFile $projectIncludes $systemIncludes \$systemIncludes"
     else
         exit 0
     fi
+cat >> ${outFile} <<-END
+# compile ${tsName}
+    systemIncludes="\$(setupSystemIncludes "${tsName}")"
+END
+
 elif [ "$1" == "link" ]; then
     shift; target="$1"
     shift; outputFile="$1"
@@ -91,6 +160,7 @@ elif [ "$1" == "link" ]; then
     parse "--input        inputFiles" \
           "--llibraries   inputLibraries" \
           "--syslibraries libraries" \
+          "--verbose verbose" \
           "--" "$@"
 
     libraries=($(implode " -l" "${libraries[@]}"))
@@ -108,6 +178,11 @@ elif [ "$1" == "link" ]; then
     else
         exit -1
     fi
+cat >> ${outFile} <<-END
+# linking ${outputFile} as ${target}
+END
+
+
 else
     exit -1
 fi
@@ -115,7 +190,7 @@ echo "compilable: true"
 lastDir=$(cat ${outFile} | grep "mkdir -p" | tail -n 1 | cut -b 10-)
 newDir="$(dirname ${outputFile})"
 if [ "${lastDir}" != "${newDir}" ]; then
-    echo "mkdir -p $(dirname ${outputFile})" >> ${outFile}
+    echo "    mkdir -p $(dirname ${outputFile})" >> ${outFile}
 fi
-echo "${call}" | sed -e 's/[[:space:]]*$//'>> ${outFile}
+echo "    ${call}" | sed -e 's/[[:space:]]*$//'>> ${outFile}
 exit 0
