968905b57afd4ef6a5335ddefe6d11695918c35b
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2022-05-15 16:42:48 +0200

    [refactor] tabs → spaces

--- tests/all.sh
+++ tests/all.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #if [ ! -e bin/busy ]; then
-#	echo "failed - bin/busy not found"
-#	echo "please execute this file from any busy build directory"
-#	exit 1
+#    echo "failed - bin/busy not found"
+#    echo "please execute this file from any busy build directory"
+#    exit 1
 #fi
 #BUSY="$(pwd)/bin/busy"
 BUSY='busy'
@@ -12,81 +12,80 @@ cd "${0%/*}"
 
 
 ( # check normal compilation of executable works
-	rm -rf testProject/build
-	mkdir -p testProject/build
-	cd testProject/build
-	$BUSY --toolchain "gcc 12.1" ../busy.yaml > /dev/null
+    rm -rf testProject/build
+    mkdir -p testProject/build
+    cd testProject/build
+    $BUSY --toolchain "gcc 12.1" ../busy.yaml > /dev/null
 
-	if [ "$(bin/app)" != "Hello World" ]; then
-		echo "failed"
-		exit 1
-	fi
-	cd ../..
-	rm -rf testProject/build
+    if [ "$(bin/app)" != "Hello World" ]; then
+        echo "failed"
+        exit 1
+    fi
+    cd ../..
+    rm -rf testProject/build
 )
 
 
 ( # check compilation works under different build folder
-	rm -rf testProject/different-build
-	mkdir -p testProject/different-build
-	cd testProject/different-build
-	$BUSY --toolchain "gcc 12.1" ../busy.yaml > /dev/null
+    rm -rf testProject/different-build
+    mkdir -p testProject/different-build
+    cd testProject/different-build
+    $BUSY --toolchain "gcc 12.1" ../busy.yaml > /dev/null
 
-	if [ "$(bin/app)" != "Hello World" ]; then
-		echo "failed"
-		exit 1
-	fi
-	cd ../..
-	rm -rf testProject/different-build
+    if [ "$(bin/app)" != "Hello World" ]; then
+        echo "failed"
+        exit 1
+    fi
+    cd ../..
+    rm -rf testProject/different-build
 )
 
 ( # check compilation works outside of project path
-	rm -rf external-build
-	mkdir -p external-build
-	cd external-build
-	$BUSY --toolchain "gcc 12.1" ../testProject/busy.yaml > /dev/null
+    rm -rf external-build
+    mkdir -p external-build
+    cd external-build
+    $BUSY --toolchain "gcc 12.1" ../testProject/busy.yaml > /dev/null
 
-	if [ "$(bin/app)" != "Hello World" ]; then
-		echo "failed"
-		exit 1
-	fi
-	cd ..
-	rm -rf external-build
+    if [ "$(bin/app)" != "Hello World" ]; then
+        echo "failed"
+        exit 1
+    fi
+    cd ..
+    rm -rf external-build
 )
 
 ( # check normal compilation of executable works  with build path given
-	rm -rf testProject/build
-	$BUSY --toolchain "gcc 12.1" --build testProject/build testProject/busy.yaml > /dev/null
+    rm -rf testProject/build
+    $BUSY --toolchain "gcc 12.1" --build testProject/build testProject/busy.yaml > /dev/null
 
-	if [ "$(testProject/build/bin/app)" != "Hello World" ]; then
-		echo "failed"
-		exit 1
-	fi
-	rm -rf testProject/build
+    if [ "$(testProject/build/bin/app)" != "Hello World" ]; then
+        echo "failed"
+        exit 1
+    fi
+    rm -rf testProject/build
 )
 
 
 ( # check compilation works under different build folder with build path given
-	rm -rf testProject/different-build
-	$BUSY --toolchain "gcc 12.1" --build testProject/different-build testProject/busy.yaml > /dev/null
+    rm -rf testProject/different-build
+    $BUSY --toolchain "gcc 12.1" --build testProject/different-build testProject/busy.yaml > /dev/null
 
-	if [ "$(testProject/different-build/bin/app)" != "Hello World" ]; then
-		echo "failed"
-		exit 1
-	fi
-	rm -rf testProject/different-build
+    if [ "$(testProject/different-build/bin/app)" != "Hello World" ]; then
+        echo "failed"
+        exit 1
+    fi
+    rm -rf testProject/different-build
 )
 
 
 ( # check compilation works outside of project path with build path given
-	rm -rf external-build
-	$BUSY --toolchain "gcc 12.1" --build external-build testProject/busy.yaml > /dev/null
+    rm -rf external-build
+    $BUSY --toolchain "gcc 12.1" --build external-build testProject/busy.yaml > /dev/null
 
-	if [ "$(external-build/bin/app)" != "Hello World" ]; then
-		echo "failed"
-		exit 1
-	fi
-	rm -rf external-build
+    if [ "$(external-build/bin/app)" != "Hello World" ]; then
+        echo "failed"
+        exit 1
+    fi
+    rm -rf external-build
 )
-
 
