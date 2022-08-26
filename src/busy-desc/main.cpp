35fe7b0d2d8dc2e51f774037f6cbfe866f2ccf3e
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2022-08-26 10:50:27 +0200

    refactor: splitting up function into multiple smaller ones

--- src/busy-desc/main.cpp
+++ src/busy-desc/main.cpp
@@ -14,49 +14,86 @@ struct Workspace {
     std::filesystem::path buildPath;
     std::filesystem::path toolchain;
     TranslationMap        allSets;
-};
 
-auto findDeps(busy::desc::TranslationSet const& root, TranslationMap const& map) {
-    auto deps = std::vector<busy::desc::TranslationSet>{};
-    auto found = std::unordered_set<std::string>{};
+    /** Returns a list of TranslationSets that tu is depending on
+     */
+    auto findDependencies(busy::desc::TranslationSet const& tu) const {
+        auto deps  = std::vector<busy::desc::TranslationSet>{};
+        auto found = std::unordered_set<std::string>{};
 
-    auto open = std::queue<std::string>{};
-    for (auto d : root.dependencies) {
-        open.emplace(d);
-    }
-    found.insert(root.name);
-
-    while (!open.empty()) {
-        auto n = open.front();
-        open.pop();
-        found.insert(n);
-        deps.push_back(map.at(n));
-        for (auto d : map.at(n).dependencies) {
+        auto open = std::queue<std::string>{};
+        for (auto d : tu.dependencies) {
             open.emplace(d);
         }
+        found.insert(tu.name);
+
+        while (!open.empty()) {
+            auto n = open.front();
+            open.pop();
+            found.insert(n);
+            deps.push_back(allSets.at(n));
+            for (auto d : allSets.at(n).dependencies) {
+                open.emplace(d);
+            }
+        }
+
+        return deps;
     }
 
-    return deps;
-}
+    /** compiles a single translation unit
+     */
+    auto translateUnit(auto tuName, auto tuPath) const {
+        auto cmd = busy::genCall::compilation(toolchain, tuName, tuPath, {"default"});
 
-auto translateUnit(auto tool, auto root, auto buildPath, auto tuPath) {
-    auto cmd = busy::genCall::compilation(tool, root, tuPath, {"default"});
-
-    auto call = std::string{fmt::format("(cd {}; {})", buildPath.string(), fmt::join(cmd, " "))};
-    auto p = process::Process{cmd, buildPath};
-    auto answer = busy::answer::parseCompilation(p.cout());
-    if (!p.cerr().empty()) {
-        throw std::runtime_error(std::string{"Unexpected error with the build system:"}
-            + std::string{p.cerr()});
+        auto call = std::string{fmt::format("(cd {}; {})", buildPath.string(), fmt::join(cmd, " "))};
+        auto p = process::Process{cmd, buildPath};
+        auto answer = busy::answer::parseCompilation(p.cout());
+        if (!p.cerr().empty()) {
+            throw std::runtime_error(std::string{"Unexpected error with the build system:"}
+                + std::string{p.cerr()});
+        }
+        return std::make_tuple(call, answer);
     }
-    return std::make_tuple(call, answer);
-}
+
+    /**
+     * setup translation set
+     */
+    void setupTranslationSet(auto const& ts, auto const& dependencies) const {
+        auto cmd = busy::genCall::setup_translation_set(toolchain, buildPath, ts, dependencies);
+        fmt::print("(cd {}; {})\n", buildPath.string(), fmt::join(cmd, " "));
+        auto p = process::Process{cmd, buildPath};
+        auto o = p.cout();
+        auto e = p.cerr();
+        std::cout << o << "\n";
+        std::cout << e << "\n";
+        std::cout << "\n";
+    }
+
+    /**
+     * finish translation set
+     */
+    void finishTranslationSet(auto const& ts, auto const& objFiles, auto const& dependencies) const {
+        auto type = [&]() -> std::string {
+            if (ts.type == "executable") {
+                return "executable";
+            } else if (ts.type == "library") {
+                return "static_library";
+            }
+            throw "unknown translation set target";
+        }();
+        auto cmd = busy::genCall::linking(toolchain, ts, type, objFiles, dependencies);
+        fmt::print("(cd {}; {})\n", buildPath.string(), fmt::join(cmd, " "));
+        auto p = process::Process{cmd, buildPath};
+        auto o = p.cout();
+        auto e = p.cerr();
+        std::cout << o << "\n";
+        std::cout << e << "\n";
+
+    }
+};
 
 void translate(busy::desc::TranslationSet const& target, Workspace const& workspace) {
-    auto const& toolchain = workspace.toolchain;
-    auto const& buildPath = workspace.buildPath;
-    auto const& allSets   = workspace.allSets;
-    auto deps = findDeps(target, allSets);
+    auto deps = workspace.findDependencies(target);
     for (auto d : deps) {
         translate(d, workspace);
     }
@@ -65,16 +102,7 @@ void translate(busy::desc::TranslationSet const& target, Workspace const& worksp
         return;
     }
 
-    {
-        auto cmd = busy::genCall::setup_translation_set(toolchain, buildPath, target, deps);
-        fmt::print("(cd {}; {})\n", buildPath.string(), fmt::join(cmd, " "));
-        auto p = process::Process{cmd, buildPath};
-        auto o = p.cout();
-        auto e = p.cerr();
-        std::cout << o << "\n";
-        std::cout << e << "\n";
-        std::cout << "\n";
-    }
+    workspace.setupTranslationSet(target, deps);
 
     std::vector<std::filesystem::path> objFiles;
     auto tsPath = target.path / "src" / target.name;
@@ -82,33 +110,13 @@ void translate(busy::desc::TranslationSet const& target, Workspace const& worksp
         if (f.is_regular_file()) {
             auto tuPath = relative(f, tsPath);
             objFiles.emplace_back(tuPath.string());
-            auto [call, answer] = translateUnit(toolchain, target, buildPath, tuPath);
+            auto [call, answer] = workspace.translateUnit(target, tuPath);
             std::cout << call << "\n";
             std::cout << answer.stdout << "\n";
             std::cout << "\n";
         }
     }
-//    auto linking(std::filesystem::path const& _tool, std::string const& _type, std::filesystem::path output, std::vector<std::filesystem::path> const& _objFiles, desc::TranslationSet const& ts, std::vector<desc::TranslationSet> const& deps) {
-
-    {
-        auto type = [&]() -> std::string {
-            if (target.type == "executable") {
-                return "executable";
-            } else if (target.type == "library") {
-                return "static_library";
-            }
-            throw "unknown translation set target";
-        }();
-        auto cmd = busy::genCall::linking(toolchain, target, type, objFiles, deps);
-        fmt::print("(cd {}; {})\n", buildPath.string(), fmt::join(cmd, " "));
-        auto p = process::Process{cmd, buildPath};
-        auto o = p.cout();
-        auto e = p.cerr();
-        std::cout << o << "\n";
-        std::cout << e << "\n";
-
-        std::cout << "\n";
-    }
+    workspace.finishTranslationSet(target, objFiles, deps);
 }
 
 int main(int argc, char const* argv[]) {
