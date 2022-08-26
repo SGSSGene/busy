a7c0343747eb6f6e9d501d8e37991904c01926ad
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2022-08-26 10:59:02 +0200

    refactor: move Toolchain related operations, into the toolchain

--- src/busy-desc/main.cpp
+++ src/busy-desc/main.cpp
@@ -9,36 +9,9 @@
 #include <unordered_map>
 #include <queue>
 
-using TranslationMap = std::unordered_map<std::string, busy::desc::TranslationSet>;
-struct Workspace {
+struct Toolchain {
     std::filesystem::path buildPath;
     std::filesystem::path toolchain;
-    TranslationMap        allSets;
-
-    /** Returns a list of TranslationSets that tu is depending on
-     */
-    auto findDependencies(busy::desc::TranslationSet const& tu) const {
-        auto deps  = std::vector<busy::desc::TranslationSet>{};
-        auto found = std::unordered_set<std::string>{};
-
-        auto open = std::queue<std::string>{};
-        for (auto d : tu.dependencies) {
-            open.emplace(d);
-        }
-        found.insert(tu.name);
-
-        while (!open.empty()) {
-            auto n = open.front();
-            open.pop();
-            found.insert(n);
-            deps.push_back(allSets.at(n));
-            for (auto d : allSets.at(n).dependencies) {
-                open.emplace(d);
-            }
-        }
-
-        return deps;
-    }
 
     /** compiles a single translation unit
      */
@@ -90,45 +63,87 @@ struct Workspace {
         std::cout << e << "\n";
 
     }
+
 };
 
-void translate(busy::desc::TranslationSet const& target, Workspace const& workspace) {
-    auto deps = workspace.findDependencies(target);
-    for (auto d : deps) {
-        translate(d, workspace);
-    }
-    std::cout << "\n\nTRANSLATING: " << target.name << "\n";
-    if (target.precompiled) {
-        return;
-    }
+using TranslationMap = std::unordered_map<std::string, busy::desc::TranslationSet>;
+struct Workspace {
+    std::filesystem::path buildPath;
+    TranslationMap        allSets;
+    Toolchain             toolchain;
 
-    workspace.setupTranslationSet(target, deps);
+    /** Returns a list of TranslationSets that tu is depending on
+     */
+    auto findDependencies(busy::desc::TranslationSet const& tu) const {
+        auto deps  = std::vector<busy::desc::TranslationSet>{};
+        auto found = std::unordered_set<std::string>{};
 
-    std::vector<std::filesystem::path> objFiles;
-    auto tsPath = target.path / "src" / target.name;
-    for (auto f : std::filesystem::recursive_directory_iterator(tsPath)) {
-        if (f.is_regular_file()) {
-            auto tuPath = relative(f, tsPath);
-            objFiles.emplace_back(tuPath.string());
-            auto [call, answer] = workspace.translateUnit(target, tuPath);
-            std::cout << call << "\n";
-            std::cout << answer.stdout << "\n";
-            std::cout << "\n";
+        auto open = std::queue<std::string>{};
+        for (auto d : tu.dependencies) {
+            open.emplace(d);
         }
+        found.insert(tu.name);
+
+        while (!open.empty()) {
+            auto n = open.front();
+            open.pop();
+            found.insert(n);
+            deps.push_back(allSets.at(n));
+            for (auto d : allSets.at(n).dependencies) {
+                open.emplace(d);
+            }
+        }
+
+        return deps;
     }
-    workspace.finishTranslationSet(target, objFiles, deps);
-}
+
+    /** Translates a tu and its dependencnies
+     */
+    void translate(busy::desc::TranslationSet const& ts) const {
+        auto deps = findDependencies(ts);
+        for (auto d : deps) {
+            translate(d);
+        }
+        std::cout << "\n\nTRANSLATING: " << ts.name << "\n";
+        if (ts.precompiled) {
+            return;
+        }
+
+        toolchain.setupTranslationSet(ts, deps);
+
+        auto objFiles = std::vector<std::filesystem::path>{};
+        auto tsPath = ts.path / "src" / ts.name;
+        for (auto f : std::filesystem::recursive_directory_iterator(tsPath)) {
+            if (f.is_regular_file()) {
+                auto tuPath = relative(f, tsPath);
+                objFiles.emplace_back(tuPath.string());
+                auto [call, answer] = toolchain.translateUnit(ts, tuPath);
+                std::cout << call << "\n";
+                std::cout << answer.stdout << "\n";
+                std::cout << "\n";
+            }
+        }
+        toolchain.finishTranslationSet(ts, objFiles, deps);
+    }
+
+
+};
 
 int main(int argc, char const* argv[]) {
     if (argc <= 1) return 1;
     // ./build/bin/busy-desc busy3.yaml build toolchains.d/gcc12.1.sh
     try {
         auto busyFile  = std::filesystem::path{argv[1]};
+        auto buildPath = std::filesystem::path{argv[2]};
+        auto toolchain = std::filesystem::path{argv[3]};
+
+        toolchain = relative(absolute(toolchain), buildPath);
+
         auto workspace = Workspace {
-            .buildPath = std::filesystem::path{argv[2]},
-            .toolchain = std::filesystem::path{argv[3]},
+            .buildPath = buildPath,
+            .toolchain = { .buildPath = buildPath,
+                           .toolchain = toolchain },
         };
-        workspace.toolchain = relative(absolute(workspace.toolchain), workspace.buildPath);
 
         std::string mainExe; //!TODO smarter way to decide this ;-)
         auto desc = busy::desc::loadDesc(busyFile, workspace.buildPath);
@@ -138,7 +153,7 @@ int main(int argc, char const* argv[]) {
             auto path = desc.path / "src" / ts.name;
         }
         auto root = workspace.allSets.at(mainExe);
-        translate(root, workspace);
+        workspace.translate(root);
 
 
     } catch (char const* p) {
