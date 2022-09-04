561c1e6101cbff10f9f45c15bb79e97141be59f1
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2022-09-04 15:08:17 +0200

    patch: move Toolchain into own file

--- src/busy-desc/main.cpp
+++ src/busy-desc/main.cpp
@@ -2,6 +2,7 @@
 #include "genCall.h"
 #include "answer.h"
 #include "Process.h"
+#include "Toolchain.h"
 
 #include <fmt/format.h>
 #include <fstream>
@@ -10,72 +11,6 @@
 #include <unordered_map>
 #include <unordered_set>
 
-struct Toolchain {
-    std::filesystem::path    buildPath;
-    std::filesystem::path    toolchain;
-    std::vector<std::string> languages;
-
-    void detectLanguages() {
-        auto cmd = std::vector<std::string>{toolchain, "info"};
-        auto p = process::Process{cmd, buildPath};
-        auto node = YAML::Load(std::string{p.cout()});
-        for (auto n : node["toolchains"]) {
-            for (auto l : n["languages"]) {
-                languages.emplace_back(l.as<std::string>());
-            }
-        }
-    }
-
-    /** compiles a single translation unit
-     */
-    auto translateUnit(auto tuName, auto tuPath) const {
-        auto cmd = busy::genCall::compilation(toolchain, tuName, tuPath, {"default"});
-
-        auto call = std::string{fmt::format("(cd {}; {})", buildPath.string(), fmt::join(cmd, " "))};
-        auto p = process::Process{cmd, buildPath};
-        auto answer = busy::answer::parseCompilation(p.cout());
-        if (!p.cerr().empty()) {
-            throw std::runtime_error(std::string{"Unexpected error with the build system:"}
-                + std::string{p.cerr()});
-        }
-        return std::make_tuple(call, answer);
-    }
-
-    /**
-     * setup translation set
-     */
-    void setupTranslationSet(auto const& ts, auto const& dependencies) const {
-        auto cmd = busy::genCall::setup_translation_set(toolchain, buildPath, ts, dependencies);
-        fmt::print("(cd {}; {})\n", buildPath.string(), fmt::join(cmd, " "));
-        auto p = process::Process{cmd, buildPath};
-        auto o = p.cout();
-        auto e = p.cerr();
-        std::cout << o << "\n";
-        std::cout << e << "\n";
-        std::cout << "\n";
-    }
-
-    /**
-     * finish translation set
-     */
-    void finishTranslationSet(auto const& ts, auto const& objFiles, auto const& dependencies) const {
-        auto type = [&]() -> std::string {
-            if (ts.type == "executable") {
-                return "executable";
-            } else if (ts.type == "library") {
-                return "static_library";
-            }
-            throw "unknown translation set target";
-        }();
-        auto cmd = busy::genCall::linking(toolchain, ts, type, objFiles, dependencies);
-        fmt::print("(cd {}; {})\n", buildPath.string(), fmt::join(cmd, " "));
-        auto p = process::Process{cmd, buildPath};
-        auto o = p.cout();
-        auto e = p.cerr();
-        std::cout << o << "\n";
-        std::cout << e << "\n";
-    }
-};
 
 using TranslationMap = std::unordered_map<std::string, busy::desc::TranslationSet>;
 struct Workspace {
