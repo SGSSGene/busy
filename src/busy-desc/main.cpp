30d54a7f6824467542dc5230c53624c7efa44202
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2022-09-04 22:21:43 +0200

    patch: mode is being parsed from the argument parser

--- src/busy-desc/main.cpp
+++ src/busy-desc/main.cpp
@@ -13,6 +13,7 @@
 #include <unordered_set>
 
 struct Arguments {
+    std::string mode;
     std::filesystem::path buildPath{"."};
     std::optional<std::filesystem::path> busyFile;
     std::vector<std::filesystem::path> addToolchains;
@@ -20,6 +21,7 @@ struct Arguments {
     Arguments(int argc, char const* const* argv) {
         auto busyFile  = std::filesystem::path{};
         auto toolchains = std::vector<Toolchain>{};
+        mode = argv[1];
         for (int i{2}; i < argc; ++i) {
             if (argv[i] == std::string_view{"-f"} and i+1 < argc) {
                 ++i;
@@ -43,20 +45,24 @@ int main(int argc, char const* argv[]) {
     if (argc < 2) return 1;
 
     // ./build/bin/busy-desc compile build -f busy3.yaml -t toolchains.d/gcc12.1.sh
-    auto mode = std::string{argv[1]};
+    auto args = Arguments{argc, argv};
 
     try {
-        if (mode == "compile") {
-            auto args = Arguments{argc, argv};
+        if (args.mode == "compile") {
 
             auto workspace = Workspace{args.buildPath};
+
+            // set new busy file if set by commandline
             if (args.busyFile) {
                 workspace.busyFile = *args.busyFile;
             }
+
+            // add more toolchains if set by commandline
             for (auto&& t : args.addToolchains) {
                 workspace.toolchains.emplace_back(args.buildPath, std::move(t));
             }
 
+            // load busyFile
             std::string mainExe; //!TODO smarter way to decide this ;-)
             auto desc = busy::desc::loadDesc(workspace.busyFile, workspace.buildPath);
             for (auto ts : desc.translationSets) {
@@ -65,8 +71,12 @@ int main(int argc, char const* argv[]) {
                 auto path = desc.path / "src" / ts.name;
             }
             auto root = workspace.allSets.at(mainExe);
+
+            // translate main executable
             workspace.translate(root);
             workspace.save();
+        } else {
+            throw std::runtime_error("unknown mode: " + args.mode);
         }
     } catch (std::exception const& e) {
         fmt::print("{}\n", e.what());
