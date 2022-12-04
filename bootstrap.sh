e6eaa605627f860030fa24bf6528cb14b4e70ce4
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2022-12-04 13:57:15 +0100

    refactor: rename busy-desc → busy

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,5 +4,5 @@ set -Eeuo pipefail
 mkdir -p bootstrap.d
 cd bootstrap.d
 
-g++ -std=c++20 ../src/busy-desc/main.cpp -lyaml-cpp -lfmt -O0 -ggdb3 -o busy
-./busy compile -f ../busy3.yaml -t ../toolchains.d/gcc12.2.sh
+g++ -std=c++20 ../src/busy/main.cpp -lyaml-cpp -lfmt -O0 -ggdb3 -o busy
+./busy compile -f ../busy.yaml -t ../toolchains.d/gcc12.2.sh
