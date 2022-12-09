272ca82de913cfe5daf61f951f9c56360f8f334f
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2022-12-09 23:28:57 +0100

    test: test for error on broken compilation unit

--- tests/all.sh
+++ tests/all.sh
@@ -15,7 +15,8 @@ set -Eeuo pipefail
 
     busy compile -f ${project}/busy.yaml -t gcc12.2
 
-    if [ "$(bin/app)" != "Hello World" ]; then
+    str="$(bin/app)";
+    if [ "${str}" != "Hello World" ]; then
         echo "failed"
         exit 1
     fi
@@ -33,7 +34,37 @@ set -Eeuo pipefail
 
     busy compile -f ${project}/busy.yaml -t gcc12.2
 
-    if [ "$(bin/app)" != "Hello World" ]; then
+    str="$(bin/app)";
+    if [ "${str}" != "Hello World" ]; then
+        echo "failed"
+        exit 1
+    fi
+    cd ..
+    rm -rf ${build_path}
+)
+
+# check compilation fail
+(
+    build_path="test-build"
+    project="../brokenSingleApp"
+    rm -rf ${build_path}
+    mkdir -p ${build_path}
+    cd ${build_path}
+
+    str="$(busy compile -f ${project}/busy.yaml -t gcc12.2 || true)"
+    exp=$(cat <<-END
+compile error: error compiling:
+[01m[Kenvironments/app/src/app/main.cpp:[m[K In function ‘[01m[Kint main()[m[K’:
+[01m[Kenvironments/app/src/app/main.cpp:4:5:[m[K [01;31m[Kerror: [m[K‘[01m[Kblub[m[K’ was not declared in this scope
+    4 |     [01;31m[Kblub[m[K; // Some invalid code
+      |     [01;31m[K^~~~[m[K
+END
+)
+
+    if [ "${str}" != "${exp}" ]; then
+        echo "${str}"
+        echo "${exp}"
+        echo $ret
         echo "failed"
         exit 1
     fi
