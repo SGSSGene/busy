a177038c64dffd7d3b2cf2f77b5832631df8d500
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2022-12-10 12:22:12 +0100

    fix: parse all busy.yaml when calling status

--- src/busy/main.cpp
+++ src/busy/main.cpp
@@ -325,21 +325,21 @@ int main(int argc, char const* argv[]) {
             auto workspace = Workspace{args.buildPath};
             updateWorkspace(workspace);
 
-            // load busyFile
-            auto desc = busy::desc::loadDesc(workspace.busyFile, workspace.buildPath);
+            // load busy files
+            auto toolchains = loadAllBusyFiles(workspace, args.verbose);
 
-            updateWorkspaceToolchains(workspace, desc.toolchains);
+            updateWorkspaceToolchains(workspace, toolchains);
 
             fmt::print("available ts:\n");
             for (auto type : {"executable", "library"}) {
                 fmt::print("  {}:\n", type);
-                for (auto ts : desc.translationSets) {
+                for (auto const& [key, ts] : workspace.allSets) {
                     if (ts.type != type) continue;
                     fmt::print("    - {}{}\n", ts.name, ts.precompiled?" (precompiled)":"");
                 }
             }
             fmt::print("available toolchains:\n");
-            for (auto [key, value] : desc.toolchains) {
+            for (auto [key, value] : toolchains) {
                 fmt::print("    {}: {}\n", key, value);
             }
             workspace.save();
