bf6bb0785a8d8daf4a795520440ee1ecd1dc101a
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2023-02-05 23:54:27 +0100

    doc: extended message when running bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,3 +7,11 @@ cd bootstrap.d
 g++ -std=c++20 ../src/busy/main.cpp -lyaml-cpp -lfmt -O0 -ggdb3 -o busy
 export BUSY_ROOT=../share/busy/fake-root/
 ./busy compile -f ../busy.yaml -t gcc12.2
+rm busy
+echo "Compiling busy was a success!"
+echo ""
+echo "add following line to your .bashrc or .zshrc:"
+echo "        "'export PATH="${HOME}/.config/busy/env/bin:${PATH+:$PATH}"'
+echo ""
+echo "and install busy by calling"
+echo "        (cd bootstrap.d; ./bin/busy install)"
