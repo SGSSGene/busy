3e98f665f5b1ede4baaf945b287d0058210aa5bd
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2023-02-05 22:01:55 +0100

    fix: buffers need to reserve at least 4096 bytes, otherwise the algo gets stuck

--- src/busy/Process.h
+++ src/busy/Process.h
@@ -83,6 +83,7 @@ private:
 
     void parentProcess(pid_t pid) {
         auto readUntilEnd = [](auto& file, auto& out) {
+            out.reserve(4096);
             while (true) {
                 auto origSize = out.size();
                 if (out.capacity() - out.size() > 4096) {
