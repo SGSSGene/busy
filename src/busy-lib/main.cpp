30535d576b0d1d1454ea2f9e0f850e51cd7e500a
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2023-02-26 11:05:12 +0100

    patch: move WorkQueue into own file

--- src/busy-lib/main.cpp
+++ src/busy-lib/main.cpp
@@ -3,10 +3,10 @@
 #include "Process.h"
 #include "Toolchain.h"
 #include "Workspace.h"
+#include "WorkQueue.h"
 #include "utils.h"
 
 #include <cassert>
-#include <clice/clice.h>
 #include <condition_variable>
 #include <fmt/format.h>
 #include <fmt/std.h>
@@ -16,85 +16,6 @@
 #include <queue>
 #include <unordered_map>
 #include <unordered_set>
-
-struct WorkQueue {
-    struct Job {
-        std::string              name;
-        ssize_t                  blockingJobs{}; // Number of Jobs that are blocking this job
-        std::function<void()>    job;
-        std::vector<std::string> waitingJobs;    // Jobs that are waiting for this job
-    };
-
-    std::mutex                 mutex;
-    std::condition_variable    cv;
-    std::map<std::string, Job> allJobs;
-    std::vector<std::string>   readyJobs;
-    ssize_t                    jobsDone{};
-
-
-    /* Inserts a job
-     * \param name: name of this job a unique identifier
-     * \param func: the function to execute to solve this job
-     * \param blockingJobs: a list of jobs that have to be finished before this one (jobs that have to be executed before this one)
-     */
-    void insert(std::string name, std::function<void()> func, std::unordered_set<std::string> const& blockingJobs) {
-        auto g = std::lock_guard{mutex};
-        for (auto const& j : blockingJobs) {
-            allJobs[j].waitingJobs.push_back(name);
-        }
-        auto& job = allJobs[name];
-        if (!job.name.empty()) {
-            throw std::runtime_error("duplicate job name \"" + name + "\", abort!");
-        }
-        job.name          = name;
-        job.blockingJobs += ssize(blockingJobs);
-        job.job           = std::move(func);
-
-        if (blockingJobs.size() == 0) {
-            readyJobs.emplace_back(name);
-        }
-    }
-
-    bool processJob() {
-        auto g = std::unique_lock{mutex};
-        if (jobsDone == allJobs.size()) {
-            return false;
-        }
-
-        if (!readyJobs.empty()) {
-            auto last = readyJobs.back();
-            readyJobs.pop_back();
-            auto const& job = allJobs.at(last);
-            g.unlock();
-//            std::cout << "processing: " << job.name << "\n";
-            job.job();
-            finishJob(job.name);
-        } else {
-            cv.wait(g);
-        }
-
-        return true;
-    }
-
-private:
-    void finishJob(std::string const& name) {
-        auto g = std::lock_guard{mutex};
-        for (auto j : allJobs.at(name).waitingJobs) {
-            allJobs.at(j).blockingJobs -= 1;
-            if (allJobs.at(j).blockingJobs == 0) {
-                readyJobs.emplace_back(j);
-            }
-        }
-        jobsDone += 1;
-        cv.notify_all();
-    }
-
-public:
-    void flush() {
-        cv.notify_all();
-    }
-};
-
 
 auto loadAllBusyFiles(Workspace& workspace, bool verbose) -> std::map<std::string, std::filesystem::path> {
     auto toolchains = std::map<std::string, std::filesystem::path>{};
